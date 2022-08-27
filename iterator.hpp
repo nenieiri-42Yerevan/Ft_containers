9e10cdd8f6b795a24d1a1f8c35ff27be961433fd
Volodya Ismailyan <nenie_iri@mail.ru>
2022-08-27 13:10:14 +0400

    add iterator traits and it's specializations

--- iterator.hpp
+++ iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 15:47:53 by vismaily          #+#    #+#             */
-/*   Updated: 2022/08/26 16:24:11 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/08/27 13:07:37 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,36 @@ template <
 	typedef Distance	difference_type;
 	typedef Pointer		pointer;
 	typedef Reference	reference;
+};
+
+template <typename Iterator>
+struct iterator_traits
+{
+	typedef typename Iterator::iterator_category	iterator_category;
+	typedef typename Iterator::value_type			value_type;
+	typedef typename Iterator::difference_type		difference_type;
+	typedef typename Iterator::pointer				pointer;
+	typedef typename Iterator::reference			reference;
+};
+
+template <class T>
+struct iterator_traits
+{
+	typedef random_access_iterator_tag				iterator_category;
+	typedef T										value_type;
+	typedef ptrdiff_t								difference_type;
+	typedef T*										pointer;
+	typedef T&										reference;
+};
+
+template <class T>
+struct iterator_traits
+{
+	typedef random_access_iterator_tag				iterator_category;
+	typedef T										value_type;
+	typedef ptrdiff_t								difference_type;
+	typedef const T*								pointer;
+	typedef const T&								reference;
 };
 
 #endif
